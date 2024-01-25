199c351e76a9572a450190203a59d7abd1bd4f25
김보해 <155736054+salgoo62@users.noreply.github.com>
2024-01-25 23:53:12 +0900

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 function toggleMenu() {
     var menu = document.getElementById('menu');
-    if (window.innerWidth > 800) {
+    if (window.innerWidth > 750) {
         menu.style.display = "flex";
     } else {
         menu.style.display = (menu.style.display === "none") ? "flex" : "none";
@@ -11,7 +11,7 @@ function toggleMenu() {
 document.addEventListener('click', function(event) {
     var menu = document.getElementById('menu');
     var menuIcon = document.querySelector('.menu-icon');
-    if (window.innerWidth <= 800) {
+    if (window.innerWidth <= 750) {
         if (event.target !== menu && event.target.parentNode !== menu && event.target !== menuIcon && event.target.parentNode !== menuIcon) {
             menu.style.display = 'none';
             menu.style.transform = "";
@@ -21,10 +21,9 @@ document.addEventListener('click', function(event) {
 
 window.addEventListener('resize', function() {
     var menu = document.getElementById('menu');
-    if (window.innerWidth > 800) {
+    if (window.innerWidth > 750) {
         menu.style.display = "flex";
     } else {
-        // 화면이 800px 이하인 경우 메뉴를 숨김
         menu.style.display = "none";
     }
 });
